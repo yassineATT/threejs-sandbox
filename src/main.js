6711560a50028093e4eead95ae7af0ab6e13b1c1
yassineATT <yassineatta@hotmail.com>
2024-09-17 01:15:49 +0200

    [ADD] Init threeJs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { World } from "./World/World";
 
+function main() {
+  const container = document.querySelector("#scene-container");
+  const world = new World(container);
+  world.start();
+}
+
+main();
 createApp(App).mount("#app");
