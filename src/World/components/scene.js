1dfc000a7cb152a5ebc7bb977d82084fa263d418
yassineATT <yassineatta@hotmail.com>
2024-09-20 00:49:09 +0200

    [ADD] fog to back plan

--- src/World/components/scene.js
+++ src/World/components/scene.js
@@ -1,8 +1,9 @@
-import { Color, Scene } from 'three';
+import { Color, Scene, Fog } from 'three';
 
 function createScene(color) {
   const scene = new Scene();
   scene.background = new Color(color);
+  scene.fog = new Fog(color, 20, 90);
   return scene;
 }
 
