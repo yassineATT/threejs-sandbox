3aa163e42487137ed4192b7e13cdb6f2f6f38174
yassineATT <yassineatta@hotmail.com>
2024-09-20 00:48:50 +0200

    [ADD] Adjust camera and light

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -2,14 +2,14 @@ import { PerspectiveCamera } from "three";
 
 function createCamera() {
   const camera = new PerspectiveCamera(
-    35, // fov = Field Of View / angle de vue
+    40, // fov = Field Of View / angle de vue
     1, // aspect ratio (dummy value) / ratio de l'écran
     0.1, // near clipping plane / distance de début de rendu
     100 // far clipping plane / distance de fin de rendu
   );
 
   // Déplacer la caméra pour qu'elle soit en face de la scène
-  camera.position.set(0, 0, 100);
+  camera.position.set(0, 10, 40);
   camera.tick = (delta) => {};
 
   return camera;
--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -3,7 +3,7 @@ import { DirectionalLight, DirectionalLightHelper } from "three";
 function createLights(color) {
   const light = new DirectionalLight(color, 4);
   const lightHelper = new DirectionalLightHelper(light, 0);
-  light.position.set(0, 0, 100);
+  light.position.set(0, 30, 30);
   light.tick = (delta) => {};
   return { light, lightHelper };
 }
